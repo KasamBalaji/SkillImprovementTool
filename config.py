e39ccca4e5eb3fc2a61f8d4def96a61f16b00897
KasamBalaji <kasam.balaji.1412@gmail.com>
2022-02-07 11:55:39 +0530

    Login Functionality Added

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ DEBUG = True
 
 # Define the application directory
 import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+BASE_DIR = os.path.abspath(os.path.dirname(__file__)) 
+SQLALCHEMY_DATABASE_URI = ''
 
 
 # Application threads. A common general assumption is
