8081939e77d02680b509ca4b5a9a4386ce8631b5
meenakshi <meenakshi2615@example.com>
2022-02-14 15:10:11 +0530

    update index ,login , register

--- app/__init__.py
+++ app/__init__.py
@@ -1,34 +1,18 @@
-from flask import Flask,render_template,redirect,url_for,session
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager,login_required
-from app.forms import SkillForm 
-
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config.from_object('config')
-app.secret_key = "abc"
 
 db = SQLAlchemy(app)
 bootstrap =Bootstrap(app)
-
 login_manager = LoginManager(app)
 login_manager.login_view = 'auth.login'
 
 from app.auth.controllers import auth
-from app.eval.controllers import eval
 app.register_blueprint(auth,url_prefix='/auth')
-app.register_blueprint(eval,url_prefix='')
 
 
-@app.route('/',methods=['GET','POST'])
-@login_required
-def index():
-    form = SkillForm()
-    if form.validate_on_submit():
-        if form.submit1.data:
-            return redirect(url_for('eval.temp',skills=form.skills.data))
-        elif form.submit2.data:
-            return redirect(url_for('eval.test',skills=form.skills.data))
-    return render_template('index.html',form=form)
