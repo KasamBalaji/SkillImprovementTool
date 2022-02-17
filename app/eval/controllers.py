83daca80f57539d699f383be54add6698e7623d7
KasamBalaji <kasam.balaji.1412@gmail.com>
2022-02-17 12:47:38 +0530

    90 percent done

--- app/eval/controllers.py
+++ app/eval/controllers.py
@@ -7,7 +7,7 @@ from sqlalchemy import text
 from sqlalchemy.sql import func
 from app.eval.codechecker import codechecker,STATUS_CODES
 from app.eval.forms import MCQForm
-import os
+import os,random
 eval = Blueprint('eval',__name__)
 
 
@@ -19,8 +19,29 @@ def daba():
     # tasks =db.session.query(TaskSkill).from_statement(text("SELECT * FROM task_skills WHERE CAST(skill_values->>'HTML' AS INTEGER)>=1000 and CAST(skill_values->>'CSS' AS INTEGER)>=1000")).all()
     # print(tasks)
     # session["qno"]=None
-    res = db.session.query(func.avg(Batches.time),func.stddev_pop(Batches.time)).filter_by(user_id=1).first()
-    print(res)
+
+    # res = db.session.query(func.avg(Batches.time),func.stddev_pop(Batches.time)).filter_by(user_id=1).first()
+    # print(res)
+
+
+    # task_id =1
+    # user_id =1
+    # result ={"answer":"TheAnswer"}
+    # submission = Submissions(task_id=task_id,user_id=user_id,result=result)
+    # db.session.add(submission)
+    # db.session.commit()
+    # db.session.flush()
+    # sub_id = submission.id
+    # batch_no=1
+    # for i in range(10):
+    #     quality = random.randint(1,101)
+    #     time = random.randint(300,500)
+    #     skillgaps = {'HTML': random.randint(500,700)}
+    #     batch = Batches(batch_number=batch_no,user_id=user_id,task_id=task_id,submission_id=sub_id,quality=quality,time=time,skills=skillgaps)
+    #     db.session.add(batch)
+    #     db.session.commit()
+    res=  db.engine.execute("SElECT percent_rank FROM  (SELECT time, PERCENT_RANK() OVER(order by time) FROM batches where task_id={}) pt where time={}".format(1,322)).fetchall()
+    print(res[0][0])
     return 'Hello'
 
 class Problem(View):
@@ -96,10 +117,95 @@ eval.add_url_rule('/temp',view_func=Problem.as_view('temp'))
 eval.add_url_rule('/test',view_func=Problem.as_view('test'))
 
 
+def create_submission(task_id,user_id,result):
+    submission = Submissions(task_id=task_id,user_id=user_id,result=result)
+    db.session.add(submission)
+    db.session.commit()
+    db.session.flush()
+    return submission.id
+
+
 def create_file(filename,content):
     text_file = open(filename,"w")
     text_file.write(content)
     text_file.close()
+
+def update_skill_batch(user_id,task_id,sub_id,quality,time,type):
+    #Get User Object
+    user = User.query.filter_by(id=user_id).first()
+    userskill = UserSkill.query.filter_by(user_id=user_id).first()
+    user_skill_values = userskill.skill_values
+    #Get Task Skills Object
+    taskskill = TaskSkill.query.filter_by(task_id=task_id).first()
+    task_skill_values = taskskill.skill_values
+
+    #Get Skill Gaps for Skill in taskskill
+    skillgaps ={}
+    for skill in task_skill_values:
+        skillgaps[skill]=task_skill_values[skill]-user_skill_values[skill]
+    
+    #Batch Number
+    batch_no = user.batches_completed +1
+    #Create a Batch
+    batch = Batches(batch_number=batch_no,user_id=user_id,task_id=task_id,submission_id=sub_id,quality=quality,time=time,skills=skillgaps)
+    db.session.add(batch)
+    db.session.commit()
+
+    #Get Score using percentile
+    percentile=  db.engine.execute("SElECT percent_rank FROM  (SELECT time, PERCENT_RANK() OVER(order by time) FROM batches where task_id={}) pt where time={}".format(1,322)).fetchall()[0][0]
+    percentile = 1- percentile
+    score = quality*percentile
+    #Update Skills
+
+    user_skill_cnts = userskill.skillcnts
+    task_cnt = taskskill.cnt
+
+    task_k = 100/sqrt(1+task_cnt)
+
+    for skill in task_skill_values:
+        t_rating = task_skill_values[skill]
+        u_rating = user_skill_values[skill]
+        user_k = 100/sqrt(1+ user_skill_cnts[skill])
+        exp = t_rating- u_rating
+        expected_score =  1/((10.0**(exp))+1)
+
+        if task_cnt>=50 or type=='gtype':
+            user_skill_values[skill] = u_rating + user_k*(score- expected_score)
+            user_skill_cnts[skill] +=1
+            
+        
+        if type!='gtype':
+            task_skill_values[skill] = t_rating - task_k*(score-expected_score)
+    
+    if type!='gtype':
+        task_cnt +=1
+
+    userskill.skill_values = user_skill_values
+    userskill.skillcnts = user_skill_cnts
+
+    taskskill.cnt = task_cnt
+    taskskill.skill_values = task_skill_values
+
+    db.session.add(userskill)
+    db.session.add(taskskill)
+    db.session.commit()
+        
+
+            
+
+
+
+    #Update Skill based on type
+    # for skill in task_skill_values:
+
+
+
+
+
+
+    
+
+
 
 @login_required
 @eval.route('/submit',methods=['POST'])
@@ -113,6 +219,7 @@ def submit():
         q_code = session["task_q_code"]
         e_code = session["task_e_code"]
         task_id = session["task_ids"][qno-1]
+        user_id = session["user_id"]
         
         print(q_code)
         print(e_code)
@@ -148,6 +255,9 @@ def submit():
                     return results
                 quality = cnt/len(inputs)
                 time = 500
+                result = {"answer":code}
+                sub_id =create_submission(task_id,user_id,result)
+                update_skill_batch(user_id,task_id,sub_id,quality,time,type)
 
                 
                 
@@ -161,6 +271,10 @@ def submit():
                 else:
                     quality =0
                 time = 500
+                print(quality)
+                result = {"answer":answer}
+                sub_id =create_submission(task_id,user_id,result)
+                update_skill_batch(user_id,task_id,sub_id,quality,time,type)
         
         if q_code=='qc4':
             print(data)
@@ -171,6 +285,10 @@ def submit():
                 else:
                     quality=0
                 time = 500
+                print(quality)
+                result = {"answer":answer}
+                sub_id =create_submission(task_id,user_id,result)
+                update_skill_batch(user_id,task_id,sub_id,quality,time,type)
 
 
 
